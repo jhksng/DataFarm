12348003d95db759c0ace38df1c2e036262ae892
jhksng <jeonghokim2727@gmail.com>
2025-09-26 11:50:31 +0900

    Update controller.py

--- Project/RaspberryPi/controller.py
+++ Project/RaspberryPi/controller.py
@@ -11,56 +11,55 @@ GPIO.setmode(GPIO.BCM)
 for pin in RELAY_PINS:
     GPIO.setup(pin, GPIO.OUT)
 
-# 릴레이 모듈을 사용해서 LOW 일 때 ON
 def set_pin_state(pin, state):
     if state == 1: # ON
-        GPIO.output(pin, GPIO.LOW)
+        GPIO.output(pin, GPIO.HIGH)
         print(f"GPIO {pin} PIN ON")
     elif state == 0: # OFF
-        GPIO.output(pin, GPIO.HIGH)
+        GPIO.output(pin, GPIO.LOW)
         print(f"GPIO {pin} PIN OFF")
 
 def main():
     try:
-        # 모든 핀을 초기 상태(꺼짐)로 설정
+        # 핀 초기 상태 OFF
         print("모든 릴레이 핀을 초기화합니다.")
         for pin in RELAY_PINS:
-            GPIO.output(pin, GPIO.HIGH)
-        
+            GPIO.output(pin, GPIO.LOW)
+            
         while True:
-            # 현재 상태 출력
+           
             print("\n--- 현재 릴레이 상태 ---")
             for pin in RELAY_PINS:
                 # 릴레이가 Low일 때 켜진 상태로 표시
-                current_state = "켜짐" if GPIO.input(pin) == GPIO.LOW else "꺼짐"
+                current_state = "켜짐" if GPIO.input(pin) == GPIO.HIGH else "꺼짐"
                 print(f"GPIO {pin} 핀: {current_state}")
             print("------------------------")
 
             # 사용자 입력 받기
-            print("\n어떤 핀의 상태를 변경하시겠습니까? (5, 6, 13, 19, 26) 'q'를 입력하면 종료합니다.")
+            print("\n INPUT PIN NUMBER (5, 6, 13, 19, 26) EXIT BUTTON 'e'")
             pin_input = input("핀 번호 입력: ")
 
-            if pin_input.lower() == 'q':
+            if pin_input.lower() == 'e':
                 break
 
             try:
                 pin_to_control = int(pin_input)
                 if pin_to_control not in RELAY_PINS:
-                    print("⚠️ 유효하지 않은 핀 번호입니다. 다시 입력해 주세요.")
+                    print("다시 입력해 주세요.")
                     continue
 
-                print(f"GPIO {pin_to_control} 핀을 어떻게 할까요? (1: 켜기, 0: 끄기)")
-                state_input = input("상태 입력 (1 또는 0): ")
+                print(f"GPIO {pin_to_control} 핀 (1: 켜기, 0: 끄기)")
+                state_input = input("(1: 켜기, 0: 끄기): ")
 
                 state_to_set = int(state_input)
                 if state_to_set not in [0, 1]:
-                    print("⚠️ 유효하지 않은 상태 값입니다. 1 또는 0을 입력해 주세요.")
+                    print("1 또는 0을 입력해 주세요.")
                     continue
 
                 set_pin_state(pin_to_control, state_to_set)
 
             except ValueError:
-                print("⚠️ 잘못된 입력입니다. 숫자를 입력해 주세요.")
+                print("숫자를 입력해 주세요.")
                 
     except KeyboardInterrupt:
         print("\n프로그램을 종료합니다.")
