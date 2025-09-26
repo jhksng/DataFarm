ebf45d58c0f1ee5d64f09562d65a4ea28c5fe0eb
jhksng <jeonghokim2727@gmail.com>
2025-09-26 02:42:21 +0000

    Add files via upload

---
+++ Project/SpringBootServer/datafarm-server/src/test/java/com/smartfarm/smartfarm_server/SmartfarmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartfarm.smartfarm_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartfarmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
