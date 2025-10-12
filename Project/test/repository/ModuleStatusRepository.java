2a3950dc4571acb5c146779e396aa7706efda47e
htththt1 <134276734+htththt1@users.noreply.github.com>
2025-10-12 22:19:12 +0900

    Delete Project/test/repository directory

--- Project/test/repository/ModuleStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartfarm.smartfarm_server.repository;
-
-import com.smartfarm.smartfarm_server.model.ModuleStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ModuleStatusRepository extends JpaRepository<ModuleStatus, Long> {
-    Optional<ModuleStatus> findByDeviceIdAndModuleName(String deviceId, String moduleName);
-}
