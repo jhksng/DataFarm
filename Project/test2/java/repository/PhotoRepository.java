50f65248a51221fefa62c4aadc9ff758c4f68d6c
jhksng <jeonghokim2727@gmail.com>
2025-10-27 10:16:36 +0900

    Delete Project/test2 directory

--- Project/test2/java/repository/PhotoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartfarm.smartfarm_server.repository;
-
-import com.smartfarm.smartfarm_server.model.Photo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PhotoRepository extends JpaRepository<Photo, Long> {
-}
